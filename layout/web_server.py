2640903fc6fd57c16ddab9ab8db07167ad61d738
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2025-03-27 07:43:55 +0300

    second and additional tasks

--- layout/web_server.py
+++ layout/web_server.py
@@ -1,10 +1,13 @@
 # Импорт встроенной библиотеки для работы веб-сервера
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
+import os
 
 # Для начала определим настройки запуска
 hostName = "localhost"  # Адрес для доступа по сети
 serverPort = 8080  # Порт для доступа по сети
+
+#  формирование абсолютного пути к файлу с Html страницей
+PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "contacts_remote.html")
 
 
 class MyServer(BaseHTTPRequestHandler):
@@ -15,7 +18,7 @@ class MyServer(BaseHTTPRequestHandler):
 
     def __get_contacts(self):
         try:
-            with open('contacts.html', encoding='utf-8') as file:
+            with open(PATH_TO_FILE, encoding='utf-8') as file:
                 content = file.read()
                 return content
         except FileNotFoundError:
@@ -29,13 +32,23 @@ class MyServer(BaseHTTPRequestHandler):
         self.end_headers()  # Завершение формирования заголовков ответа
         self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        response = f"Received POST data: {post_data.decode('utf-8')}"
+        print(post_data.decode('utf-8'))
+        self.wfile.write(response.encode('utf-8'))
+
 
 if __name__ == "__main__":
     #  Инициализация веб-сервера, который будет по заданным параметрах в сети
     #  принимать запросы и отправлять их на обработку специальному классу, который был описан выше
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
-
+    print(PATH_TO_FILE)
     try:
         # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
         webServer.serve_forever()
